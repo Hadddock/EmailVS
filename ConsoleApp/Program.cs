9750f3e636ed2e57548c5e976df3ca862f45a277
Justin Haddock <jhadddock@protonmail.com>
2022-12-12 18:52:43 -0800

    Refactor EmailSender

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello world");
+﻿using Hadddock.Email;
+
+namespace Dummy
+{
+    public class ConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+                throw new ArgumentNullException(nameof(args),
+                    "Recipient address, message subject, " +
+                    "and message body Required as arguments");
+
+            IEmailService e = new EmailSender();
+            e.SendEmail(args[0], args[1], args[2]);
+        }
+    }
+}
--- Email/EmailSender.cs
+++ Email/EmailSender.cs
@@ -4,19 +4,12 @@ using System.Net.Mail;
 
 namespace Hadddock.Email
 {
-    public abstract class EmailService
+    public interface IEmailService
     {
-        protected IConfiguration _configuration;
-        public EmailService(IConfiguration configuration)
-        {
-            if (configuration is null)
-                throw new ArgumentNullException(nameof(configuration));
-            _configuration = configuration;
-        }
-        public abstract void SendEmail(string recipientAddress, string subject, string body);
+        public void SendEmail(string recipientAddress, string subject, string body);
     }
 
-    public class EmailSender : EmailService
+    public class EmailSender : IEmailService
     {
         private const string SENDER_NAME = "DO NOT REPLY";
         private const int MAX_ATTEMPTS = 3;
@@ -24,22 +17,33 @@ namespace Hadddock.Email
         private readonly int smtpClientPort;
         private readonly string? smtpClientLogin;
         private readonly string? smtpClientPassword;
-        public EmailSender(IConfiguration configuration) : base(configuration)
+        public EmailSender()
         {
-            if (configuration["smtpClientServer"] is null)
-                throw new ArgumentException("smtpClientServer not found in configuration");
+            string appsettingsPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)
+                + Path.DirectorySeparatorChar + "appsettings.json";
 
-            if (configuration["smtpClientPort"] is null)
-                throw new ArgumentException("smtpClientPort not found in configuration");
+            if (!File.Exists(appsettingsPath))
+            {
+                throw new FileNotFoundException("appsettings.json not found in execution directory");
+            }
 
-            if (configuration["smtpClientLogin"] is null)
-                throw new ArgumentException("smtpClientLogin not found in configuration");
+            IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true);
+            IConfigurationRoot root = builder.Build();
+            Console.WriteLine(root["smtpClientServer"]);
 
+            if (root["smtpClientServer"] is null)
+                throw new KeyNotFoundException("smtpClientServer key not found in appsettings.json");
 
-            this.smtpClientServer = configuration["smtpClientServer"];
-            this.smtpClientLogin = configuration["smtpClientLogin"];
-            this.smtpClientPassword = configuration["smtpClientPassword"];
-            string? smtpClientPortString = configuration["smtpClientPort"];
+            if (root["smtpClientPort"] is null)
+                throw new KeyNotFoundException("smtpClientPort key not found in appsettings.json");
+
+            if (root["smtpClientLogin"] is null)
+                throw new KeyNotFoundException("smtpClientLogin key not found in appsettings.json");
+
+            this.smtpClientServer = root["smtpClientServer"];
+            this.smtpClientLogin = root["smtpClientLogin"];
+            this.smtpClientPassword = root["smtpClientPassword"];
+            string? smtpClientPortString = root["smtpClientPort"];
 
             if (smtpClientPortString is not null)
             {
@@ -59,7 +63,7 @@ namespace Hadddock.Email
             }
 
         }
-        public override void SendEmail(string recipientAddress, string subject, string body)
+        public void SendEmail(string recipientAddress, string subject, string body)
         {
             if (recipientAddress is null)
                 throw new ArgumentNullException(nameof(recipientAddress));
